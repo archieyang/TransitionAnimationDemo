7851d34222de3ebc17ba081c85b74f2d95aa0f94
Archie <yangyxcn@gmail.com>
2015-06-21 17:22:03 +0800

    Implement basic transition animations
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- app/src/main/java/me/codethink/transitionanimationdemo/SecondActivity.java
+++ app/src/main/java/me/codethink/transitionanimationdemo/SecondActivity.java
@@ -1,18 +1,45 @@
 package me.codethink.transitionanimationdemo;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by archie on 15/6/21.
  */
 public class SecondActivity extends AppCompatActivity {
+    private View rootView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
+
+        rootView = findViewById(R.id.root);
+
+        if (savedInstanceState == null) {
+            rootView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    rootView.getViewTreeObserver().removeOnPreDrawListener(this);
+                    startRootAnimation();
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void startRootAnimation() {
+        rootView.setScaleY(0.1f);
+        rootView.setPivotY(rootView.getY() + rootView.getHeight() / 2);
+
+        rootView.animate()
+                .scaleY(1)
+                .setDuration(1000)
+                .setInterpolator(new AccelerateInterpolator())
+                .start();
     }
 }
